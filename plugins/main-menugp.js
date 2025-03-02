37559e5e4840c1a9a04bb158c5d3c8abc2ad4c3c
mysticclub <seishironagi.rukasproyect@gmail.com>
2025-03-01 20:59:12 -0500

    Actualizar main-menugp.js

--- plugins/main-menugp.js
+++ plugins/main-menugp.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
 'grupo': 'Menu Para Admins',
 }
-let img = 'https://files.catbox.moe/w3s79j.jpg'
+let img = 'https://i.ibb.co/CKFj3Hcc/file.jpg'
 
   try {
           // DEFAULT MENU
